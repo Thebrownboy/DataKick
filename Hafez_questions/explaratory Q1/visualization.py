f658678185a9d3df75ca8b349f324cd4abedd36e
AhmedMahmoudHafez <ahmed.mahmoud1752000@gmail.com>
2023-05-15 02:24:23 +0300

    EXPLORATORY_Q1 DONE

--- Hafez_questions/explaratory Q1/visualization.py
+++ Hafez_questions/explaratory Q1/visualization.py
@@ -6,7 +6,7 @@ import seaborn as sns
 df = pd.read_csv('./matches_shots_possession.csv')
 df_copy = df.copy()
 
-def plot_bar(df_copy):
+def plot_bar_poss(df_copy):
     avg_poss = df_copy.groupby('Result')['Poss'].mean()
     plt.figure(figsize=(10, 10))
     plt.bar(avg_poss.index, avg_poss.values, color=[
@@ -54,10 +54,29 @@ def plot_relation_res_SOT(df):
     plt.title('Distribution of shots on target by Result')
     plt.savefig('./output plots/SOT_res.png')
     plt.show()
+
+def plot_bar_SOT(df_copy):
+    avg_SOT = df_copy.groupby('Result')['SOT'].mean()
+    plt.figure(figsize=(10, 10))
+    plt.bar(avg_SOT.index, avg_SOT.values, color=[
+            '#FF8C00', '#228B22', '#4169E1'])
+    plt.title('Average shots on target for Result Categories')
+    plt.xlabel('Result')
+    plt.ylabel('Average shots on target')
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.grid(axis='y', linestyle='--', alpha=0.5)
+    plt.gca().set_facecolor('#F5F5F5')
+    for i, v in enumerate(avg_SOT.values):
+        plt.text(i, v + 1, str(round(v, 2)), ha='center',
+                fontsize=12, fontweight='bold')
+        
+    plt.savefig('./output plots/avg_SOT.png')
     
 
 # df_augmented = increase_data(df_copy)
-plot_bar(df_copy)
+plot_bar_poss(df_copy)
 plot_relation_res_pos(df_copy)
 plot_relation_res_SOT(df_copy)
+plot_bar_SOT(df_copy)
 
