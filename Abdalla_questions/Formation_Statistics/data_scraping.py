31a4678bbcb8fa1397718af8c1a6ee2e667fc7d3
Thebrownboy <abdallaxpmostafa@gmail.com>
2023-05-18 07:05:17 +0300

    updating the questions and adding more data

--- Abdalla_questions/Formation_Statistics/data_scraping.py
+++ Abdalla_questions/Formation_Statistics/data_scraping.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 import pandas as pd 
 import numpy as np 
 
-season_link = "https://fbref.com/en/comps/9/2021-2022/2008-2009-Premier-League-Stats"
+season_link = "https://fbref.com/en/comps/9/2021-2022/2021-2022-Premier-League-Stats"
 browser= webdriver.Chrome(ChromeDriverManager().install())
 
 
@@ -20,7 +20,7 @@ try :
 except:
     pass
 
-browser.set_page_load_timeout(10)
+browser.set_page_load_timeout(15)
 
 
 
@@ -34,6 +34,7 @@ for i in teams_links:
     teams_names.append(i.text)
     teams_href.append(i.get_attribute("href"))
 
+print(teams_names)
 for i ,j in zip (teams_href,teams_names) : 
     browser.get(i)
     try : 
@@ -41,16 +42,20 @@ for i ,j in zip (teams_href,teams_names) :
     except:
         pass 
 
-    browser.implicitly_wait(1)
+    browser.implicitly_wait(5)
 
     browser.find_element(By.XPATH,"/html/body/div[2]/div[6]/div[7]/div[1]/div/ul/li[1]/span").click()
+    print("yes")
+
 
     browser.implicitly_wait(1)
     browser.find_element(By.XPATH,"/html/body/div[2]/div[6]/div[7]/div[1]/div/ul/li[1]/div/ul/li[3]/button").click()
+    print("Yes")
+    browser.implicitly_wait(1)
 
     content = browser.find_element(By.XPATH,"/html/body/div[2]/div[6]/div[7]/div[2]/div/pre").text
 
-    with open(f"{j}.csv","w") as file : 
+    with open(f"{j}2.csv","w") as file : 
         file.write(content)
 
 
